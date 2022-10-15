fbf18511f90039d22d60b64a0287059514780006
Chris Marcellino <chris@marcellino.org>
2022-10-14 21:30:42 -0500

    First functional graph drawing (single graph, no time adjustment)

--- GUI.py
+++ GUI.py
@@ -6,6 +6,7 @@ import numpy
 
 class GUI:
     DATA_POINTS_TO_GRAPH = 1
+    MAIN_PLOT_TIME_WIDTH = 10 # seconds
 
     def __init__(self):
         self.view = pyqtgraph.GraphicsView()
@@ -13,13 +14,13 @@ class GUI:
         self.view.closeEvent = self.layout.closeEvent = QApplication.instance().exit(0)
         self.view.setCentralItem(self.layout)
         self.view.setWindowTitle('impedance')
-        self.view.resize(800, 600)
+        self.view.resize(800, 600)      # TODO persist last size? must be an automatic way to do so?
         self.plot_list = []
 
     def showUI(self):
         for i in range(self.DATA_POINTS_TO_GRAPH):
             new_plot = self.layout.addPlot()
-            new_plot.plot(numpy.zeros(1000 * 5))
+            new_plot.plot(numpy.zeros(1000 * self.MAIN_PLOT_TIME_WIDTH))   # this is assuming ms samples TODO
             self.plot_list.append(new_plot.listDataItems()[0])
             self.layout.nextRow()
 
@@ -27,14 +28,8 @@ class GUI:
 
     def data_callback(self, t, v):
         stream_data = [v]  # TODO ignore t for now but obviously we should to graph this more accurately at some point
-        for data, line in zip(stream_data, self.plot_list):
-            line.informViewBoundsChanged()
+        for newData, line in zip(stream_data, self.plot_list):
             xData = numpy.arange(len(line.yData))
             yData = numpy.roll(line.yData, -1)
+            yData[-1] = newData
             line.setData(xData, yData)
-            line.yData[-1] = data
-            line.xClean = line.yClean = None
-            line.xDisp = None
-            line.yDisp = None
-            line.updateItems()
-            line.sigPlotChanged.emit(line)
